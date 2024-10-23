df78d4e99fe2643b512b9c704e305e4a18035fd4
Sidney Andrews <sidandrews@microsoft.com>
2024-10-23 18:17:02 -0400

    Add client creation logic and placeholders

--- src/js/cosmos.js
+++ src/js/cosmos.js
@@ -1,6 +1,36 @@
 import { DefaultAzureCredential } from '@azure/identity';
+import { TableServiceClient, TableClient } from '@azure/data-tables';
 
 export async function start(emit) {
+    // <create_client>
     const endpoint = process.env.AZURE_COSMOS_DB_TABLE_ENDPOINT;
     console.log(`ENDPOINT: ${endpoint}`);
+
+    const credential = new DefaultAzureCredential();
+    
+    const client = new TableServiceClient(endpoint, credential);
+    // </create_client>
+    emit('Current Status:\tStarting...')
+    
+    const table = new TableClient(endpoint, "cosmicworks-products", credential);
+
+    emit(`Get table:\t${table.tableName}`);
+
+    {
+        // Create entity
+    }
+
+    {
+        // Create entity
+    }
+
+    {
+        // Read entity
+    }
+
+    {
+        // Query entities
+    }
+
+    emit('Current Status:\tFinalizing...');
 }
